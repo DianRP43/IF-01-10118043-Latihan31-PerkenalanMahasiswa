3cb622b55fd0be4cd510c2f2d792adba96556848
Dian Rosa Pratama <55683288+DianRP43@users.noreply.github.com>
2019-10-17 07:17:19 +0700

    Add files via upload

---
+++ IF-01-10118043-Latihan31-PerkenalanMahasiswa/src/Mahasiswa.java
@@ -0,0 +1,15 @@
+
+
+public class Mahasiswa {
+
+	public String nim;
+	public String nama;
+
+	public void perkenalanDiri() {
+	    System.out.println("Hallo Everyone");
+            System.out.println("My NIM is " + nim);
+            System.out.println("My name is " + nama);
+            System.out.println();
+	}
+
+}
---
+++ IF-01-10118043-Latihan31-PerkenalanMahasiswa/src/main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Mahasiswa mhs = new Mahasiswa();
+           
+            
+            //output
+            mhs. nim = "10118043";
+            mhs.nama ="Dian Rosa";
+            mhs.perkenalanDiri();
+            
+            mhs.nim = "10118001";
+            mhs.nama = "A";
+            mhs.perkenalanDiri();
+            
+            mhs.nim = "10118002";
+            mhs.nama = "B";
+            mhs.perkenalanDiri();
+            
+            mhs.nim = "10118003";
+            mhs.nama = "C";
+            mhs.perkenalanDiri();
+            
+    }
+    
+}
